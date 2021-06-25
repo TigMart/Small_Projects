d38bdaea1ec54342fe5499e665c5db11a7c3e829
Tigran <77511200+TigMart@users.noreply.github.com>
2021-06-25 15:53:06 +0400

    Main

---
+++ Address_Book/Main.cpp
@@ -0,0 +1,14 @@
+// Address_Book.cpp :
+
+#include <iostream>
+#include "Address_book.h"
+
+
+
+int main()
+{
+    std::cout << "\t\t\tADDRESS BOOK" << std::endl;
+    Address_book a;
+    a.menu();
+    return 0;
+}
